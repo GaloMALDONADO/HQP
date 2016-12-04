b1d0fb06ab50ad1f280b96af5b0b8670b2e93d32
GaloM <galo_xav@hotmail.com>
2016-12-04 17:50:36 +0100

    change the model path

--- robot_config.py
+++ robot_config.py
@@ -1,5 +1,6 @@
 import numpy as np
-model_path = '/local/gmaldona/devel/biomechatronics/models/GX.osim'
+#model_path = '/local/gmaldona/devel/biomechatronics/models/GX.osim'
+model_path = '/Models/whole_body/wholebody.osim'
 dt = 1e-3
 half_sitting = np.matrix([0.,0.,0.92,0.,0.,0.,0.,                                                       # Free flyer 0-6
                           0.09980222, -0.02487251, -0.00249558, 0.99469324, -0.4, 0.25, 0., -0.05,      # Rleg 7-14
