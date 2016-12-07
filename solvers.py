38918b732418d06851cfe7ef25c5df0f3e766919
Galo Maldonado <gmaldona@laas.fr>
2016-12-07 16:43:12 +0100

    solvers for 1st and 2nd order nullspace projector added and tested + error in SE3 corrected

--- solvers.py
+++ solvers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from wrapper import Wrapper
+import scipy
 #import
 class NProjections:
     #def __init__(self, name, q, v, dt, robotName, model_path):
@@ -17,9 +18,9 @@ class NProjections:
         self.t = 0.0
         self.tasks = []
         self.task_weights = []
-    
+        self.sot = []
 
-    def null(A, eps=1e-12):
+    def null(self, A, eps=1e-12):
         '''Compute a base of the null space of A.'''
         u, s, vh = np.linalg.svd(A)
         padding = max(0,np.shape(A)[1]-np.shape(s)[0])
@@ -33,7 +34,8 @@ class NProjections:
         '''
         self.tasks        += [task]
         self.task_weights += [weight]
-
+        #self.sot.append(task.name)
+        
     def removeTask(self, task_name):
         for (i,t) in enumerate(self.tasks):
             if t.name==task_name:
@@ -49,45 +51,88 @@ class NProjections:
         ERRstack contain a stack of desired velocities in the operational space
         '''
         Jstack = []
-        Errstack = []
+        ERRstack = []
+        Z = []
         if len(self.tasks)==1:
             J, E = self.tasks[0].kin_value(t, self.robot.q)
-            print J
-            print E
             q_dot = np.linalg.pinv(J)*E
-            #Z = self.null(J)
             return q_dot
 
         else:
             for i in xrange (len(self.tasks)):
-                task = np.vstack([self.tasks[i]])
-                Jstack, ERRstack = self.tasks[i].kin_value(t, self.robot.q)
-                q_dot = np.linalg.pinv(Jstack[0])*ERRstack[0]
-                Z = self.null(Jstack[0])
+                #_Stack jacobians and task functions
 
-        if len(Jstack) > 0:
+                # if tasks are combined at same hierarchy
+                if np.size(self.tasks[i]) > 1:
+                    j = []
+                    e = []
+                    for w in xrange(len(self.tasks[i])):
+                        x1, x2 = self.tasks[i][w].kin_value(t, self.robot.q)
+                        j.append(x1)
+                        e.append(x2)
+                        J = np.vstack(j)
+                        E = np.vstack(e)
+                        
+                else :        
+                    J, E = self.tasks[i].kin_value(t, self.robot.q)
+                    
+                # stack them
+                Jstack.append(J)
+                ERRstack.append(E)
+
+            #_Solve HQP
+            q_dot = np.linalg.pinv(Jstack[0])*ERRstack[0]
+            Z = self.null(Jstack[0]) 
             for k in range (1, len(Jstack)):
-                Jplus = Z[k-1]*np.linalg.pinv(Jstack[k]*Z[k-1])
-                x_dot = (ERRstack[k] - Jstack[k]*qdot[k-1])
+                Jplus = Z*np.linalg.pinv(Jstack[k]*Z) 
+                x_dot = (ERRstack[k] - Jstack[k]*q_dot) 
                 q_dot += Jplus*x_dot
-                Z = self.null(Jplus) 
+                Z = self.null(Jstack[k]*Z*Z.T) 
+
         return q_dot
     
-    def inverseKinematics2nd(self, Jstack, ERRstack, drift):
+    def inverseKinematics2nd(self, t):
         ''' 
         Hierarchichal Inverse Kinematics formulation based on nullspace projection
         for q_dot_dot
         ERRstack contain the desired accelerations in the operation space
         '''
-        q_dot_dot = np.linalg.pinv(Jstack[0]) * ( ERRstack[0] - drift)
-        Z = self.null(Jstack[0])
-        if len(Jstack) > 0:
+        Jstack = []
+        ERRstack = []
+        Dstack = []
+        Z = []
+        if len(self.tasks)==1:
+            J, D, E = self.tasks[0].dyn_value(t, self.robot.q, self.robot.v)
+            q_dot_dot = np.linalg.pinv(J)*(E - D)
+            return q_dot_dot
+
+        else:
+            for i in xrange (len(self.tasks)):
+                #_Stack jacobians and task functions
+
+                # if tasks are combined at same hierarchy
+                if np.size(self.tasks[i]) > 1:
+                    j = []; e = []; d =[]
+                    for w in xrange(len(self.tasks[i])):
+                        x1, x2, x3 = self.tasks[i][w].dyn_value(t, self.robot.q, self.robot.v)
+                        j.append(x1); d.append(x2); e.append(x3);
+                        J = np.vstack(j); D = np.vstack(d); E = np.vstack(e);
+                        
+                else :        
+                    J,D, E = self.tasks[i].dyn_value(t, self.robot.q, self.robot.v)
+                # stack them
+                Jstack.append(J); Dstack.append(D); ERRstack.append(E);
+
+            #_Solve HQP
+            q_dot_dot = np.linalg.pinv(Jstack[0])*(ERRstack[0]-Dstack[0])
+            Z = self.null(Jstack[0]) 
             for k in range (1, len(Jstack)):
-                Jplus = Z[k-1]*np.linalg.pinv(Jstack[k]*Z[k-1])
-                x_dot = (ERRstack[k] - Jstack[k]*qdot[k-1])
-                q_dot += Jplus*x_dot
-                Z = self.null(Jplus) 
-        return q_dot
+                Jplus = Z*np.linalg.pinv(Jstack[k]*Z) 
+                x_dot_dot = (ERRstack[k] - Jstack[k]*q_dot_dot) 
+                q_dot_dot += Jplus*(x_dot_dot-Dstack[k])
+                Z = self.null(Jstack[k]*Z*Z.T) 
+
+        return q_dot_dot
 
     def inverseDynamics(self, robot, Jstack, drift, ERRstack):
         ''' 
