5dc22c87a3830f193dce7fe437c713ca40d49d9a
Galo Maldonado <gmaldona@laas.fr>
2016-12-07 16:47:51 +0100

    updateRobotConfig updated removing useless display, calling the display from the wrapper instead

--- viewer_utils.py
+++ viewer_utils.py
@@ -46,34 +46,7 @@ class Viewer(object):
         if refresh: self.robot.viewer.gui.refresh()
 
     def updateRobotConfig(self, q, robotName, osimref=True):
-        #display visuals                            
-        # update q kinematics                              
-        #self.robots[robotName].display(q)
-        #se3.computeJacobians(self.model, self.data, q)
-        #se3.forwardKinematics(self.robot.model, self.robot.data, q)
         self.robot.display(q)
-        self.robot.q = q.copy()
-        for i in range (1,len(self.robot.visuals)):
-            # get the parent joint                                                                
-            idx = self.robot.model.getJointId(self.robot.visuals[i][1])
-            pose = se3.SE3.Identity()
-            if osimref is True:
-                # convert bones pose (Osim2Pinocchio)                    
-                pose.translation =  self.robot.data.oMi[idx].translation 
-                pose.rotation= self.robot.data.oMi[idx].rotation * self.robot.oMp
-
-            else:
-                pose.translation=  self.robot.data.oMi[idx].translation+self.robot.visuals[i][4][3:6]
-                pose.rotation= self.robot.data.oMi[idx].rotation
-
-            # place objects                                                                       
-            if i != len(self.robot.visuals)-1:
-                self.robot.viewer.gui.setScale("world/"+self.robot.visuals[i][1]+os.path.split(self.robot.visuals[i][2])[1], self.robot.visuals[i][3])
-                self.placeObject("world/"+self.robot.visuals[i][1]+os.path.split(self.robot.visuals[i][2])[1], pose, False)
-            else:
-                self.robot.viewer.gui.setScale("world/"+self.robot.visuals[i][1]+os.path.split(self.robot.visuals[i][2])[1], self.robot.visuals[i][3])
-                self.placeObject("world/"+self.robot.visuals[i][1]+os.path.split(self.robot.visuals[i][2])[1], pose, True)
-
 
     def addRobot(self, robotName, modelPath):
         if(ENABLE_VIEWER):
