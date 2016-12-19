5de40e83c4cee0041fd741846f60bb025244d5a2
Galo Maldonado <gmaldona@laas.fr>
2016-12-19 14:23:16 +0100

    add class for constant ND trajectory

--- trajectories.py
+++ trajectories.py
@@ -15,6 +15,30 @@ class RefTrajectory (object):
 
   def __call__ (self, t):
     return (np.matrix ([]).reshape (0, 0),)*3
+
+
+''' An Nd trajectory with constant state and zero velocity/acceleration. '''
+class ConstantNdTrajectory (object):
+
+  def __init__ (self, name, x_ref):
+    self._name = name
+    self._dim = x_ref.shape[0]
+    self._x_ref = np.matrix.copy(x_ref);
+    self._v_ref = np.zeros(x_ref.shape);
+    self._a_ref = np.zeros(x_ref.shape);
+
+  @property
+  def dim(self):
+    return self._dim
+    
+  def setReference(self, x_ref):
+    assert x_ref.shape[0]==self._x_ref.shape[0]
+    self._x_ref = x_ref;
+
+  def __call__ (self, t):
+    return (self._x_ref, self._v_ref, self._a_ref);
+
+
     
 ''' An se3 trajectory with constant state and zero velocity/acceleration. '''
 class ConstantSE3Trajectory (object):
