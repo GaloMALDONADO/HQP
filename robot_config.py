4428aa7e0426b005222e0a53369e3203fec53a4e
Galo Maldonado <gmaldona@laas.fr>
2016-12-19 14:25:27 +0100

    add mesh and model paths

--- robot_config.py
+++ robot_config.py
@@ -1,5 +1,4 @@
 import numpy as np
-#model_path = '/local/gmaldona/devel/biomechatronics/models/GX.osim'
 model_path = '/Models/whole_body/wholebody.osim'
 dt = 1e-3
 half_sitting = np.matrix([0.,0.,0.92,0.,0.,0.,0.,                                                       # Free flyer 0-6
@@ -9,4 +8,5 @@ half_sitting = np.matrix([0.,0.,0.92,0.,0.,0.,0.,                               
                           -0.09942936, -0.08942953,  0.00897288, 0.99097712, 0.7, 1., -0.02, 0.15, 0.,  # Rarm 31-39
                           -0.09942936,  0.08942953, -0.00897288, 0.99097712, 0.7, 1.,  0.02, 0.15, 0.]).T # Larm 40-48
 zero_pose = np.asmatrix(np.zeros(np.shape(half_sitting)))
-trajectories_path = '/local/gmaldona/devel/biomechatronics/src/tests/refs/'
+mesh_path =  '/local/gmaldona/devel/Parkour/Models/whole_body/stl_osim'
+model = '/local/gmaldona/devel/Parkour/Models/whole_body/wholebody.osim'
